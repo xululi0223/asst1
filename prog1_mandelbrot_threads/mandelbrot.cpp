185f6de4b588141fb80a77b933366fa960b721a5
kkXu <284612606@qq.com>
2025-01-21 15:48:01 +0800

    realease prog1

--- prog1_mandelbrot_threads/mandelbrot.cpp
+++ prog1_mandelbrot_threads/mandelbrot.cpp
@@ -121,6 +121,7 @@ void* workerThreadStart(void* threadArgs) {
     WorkerArgs* args = static_cast<WorkerArgs*>(threadArgs);
 
     // TODO: Implement worker thread here.
+    mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, args->width, args->height, args->threadId * args->height, (args->threadId + 1) * args->height, args->maxIterations, args->output);
 
     printf("Hello world from thread %d\n", args->threadId);
 
@@ -151,7 +152,16 @@ void mandelbrotThread(
 
     for (int i=0; i<numThreads; i++) {
         // TODO: Set thread arguments here.
+        args[i].x0 = x0;
+        args[i].x1 = x1;
+        args[i].y0 = y0;
+        args[i].y1 = y1;
+        args[i].width = width;
+        args[i].height = height / numThreads;
+        args[i].maxIterations = maxIterations;
+        args[i].output = output + i * (height / numThreads) * width;
         args[i].threadId = i;
+        args[i].numThreads = numThreads;
     }
 
     // Fire up the worker threads.  Note that numThreads-1 pthreads
